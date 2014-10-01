fa683e15e92e7ec1807cc89b958e997e3c809c3d
YEQING YAN <yyan1@mail.sfsu.edu>
2014-10-01 14:18:02 -0700

    Implement mime types configuration

--- lib/config/mime_types.rb
+++ lib/config/mime_types.rb
@@ -4,10 +4,26 @@ require_relative 'configuration'
 module WebServer
   class MimeTypes < Configuration
     def initialize(mime_file_content)
+   		
+   		@config_hash = {"random" => "text/plain"}
+
+   		mime_file_content.split("\n").each do |config_line| 
+   			config_line.strip!
+   			next if config_line.empty? || config_line[0] == '#'
+   			type,ext = config_line.split(' ', 2)
+   			if ext.split.is_a?(String)
+   				@config_hash[ext] = type
+   			else
+   				ext.split(' ').each do |item|
+					@config_hash[item] = type
+				end
+   			end
+   		end
     end
     
     # Returns the mime type for the specified extension
     def for_extension(extension)
+    	@config_hash[extension]
     end
   end
 end
