b8ab831ae4f01c7a20ab0aec107912dce9a2105b
YEQING YAN <yyan1@mail.sfsu.edu>
2014-10-01 21:24:12 -0700

    Implement resource feature

--- lib/resource.rb
+++ lib/resource.rb
@@ -1,11 +1,62 @@
 module WebServer
-  class Resource
-    attr_reader :request, :conf, :mimes
+	class Resource
+		attr_reader :request, :conf, :mimes
 
-    def initialize(request, httpd_conf, mimes)
-      @request = request
-      @conf = httpd_conf
-      @mimes = mimes
-    end
-  end
+		def initialize(request, httpd_conf, mimes)
+			@request = request
+			@conf = httpd_conf
+			@mimes = mimes
+
+		end
+
+		def resolve
+	    	# Do not add directory_index if uri end with .abc 
+	    	if @request.uri["\."].nil?
+	    		resolve_string = "#{conf.document_root}#{request.uri}/#{conf.directory_index}"
+	    	else
+	    		resolve_string = "#{conf.document_root}#{request.uri}"
+	    	end
+
+	    	# script_aliases replace
+	    	unless @conf.script_aliases.empty?
+	    		@conf.script_aliases.each do |name|
+	    			resolve_string.gsub!(name, @conf.script_alias_path(name))
+	    		end
+	    	end
+
+	    	# aliases replace
+	    	unless @conf.aliases.empty?  
+	    		@conf.aliases.each do |name|
+	    			resolve_string.gsub!(name, @conf.aliases_path(name))
+	    		end
+	    	end
+
+	    	return resolve_string
+	    end
+
+	    def script_aliased?
+
+	    	false unless @conf.script_aliases
+
+	    	@conf.script_aliases.each do |name|
+	    		if @request.uri[name]
+	    			return true
+	    		end
+	    	end
+	    	false
+	    end
+
+	    def protected?
+	    	!@request.uri["protected"].nil?
+	    end
+		
+		def authorized?(userinfo)
+			if protected?
+				(userinfo[:username] == "valid_name") && (userinfo[:password] == "valid_pwd")
+			else
+				true
+			end
+		end
+	end
 end
+
