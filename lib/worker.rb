d0bb4e7b5c6266287cc55ee0659e9810c9a235c4
YEQING YAN <yyan1@mail.sfsu.edu>
2014-10-08 15:43:08 -0700

    1. Move thread function into worker.rb2. Move httpd and mime conf to global

--- lib/worker.rb
+++ lib/worker.rb
@@ -5,13 +5,73 @@ require_relative 'response'
 # should instantiate a Request from the client (socket), perform
 # any logging, and issue the Response to the client.
 module WebServer
-  class Worker
-    # Takes a reference to the client socket and the logger object
-    def initialize(client_socket, server=nil)
-    end
 
-    # Processes the request
-    def process_request
-    end
-  end
+
+	class Worker
+
+	    # Takes a reference to the client socket and the logger object
+	    def initialize(client_socket, server=nil)
+	    	@socket = client_socket
+	    end
+
+	    # Processes the request
+	    def process_request
+	    	request = Request.new(@socket)
+
+	    	response = handle_message(request)
+
+	    	puts "---------- Response Message BEGIN -------------------"
+	    	puts response.header
+	    	puts "---------- Response Message END ---------------------"
+	    	@socket.print response.to_s
+	    end
+
+	    def handle_message (request)
+	    	response = ""
+
+	    	case request.http_method
+	    	when "GET"
+	    		response = send_resource(request)
+	    	when "HEAD"
+	    		response = generate_response(200, {})
+	    	when "POST"
+	    		response = generate_response(200, {})
+	    	when "PUT"
+	    		response = generate_response(200, {})
+	    	else
+	    		response = generate_response(404, {}) 
+	    	end                  
+	    end
+
+	    def send_resource (request)   
+	    	resource = Resource.new(request, $httpd_conf, $mime_conf)
+	    	if resource.protected? && (resource.authorized?(request.headers['AUTHORIZATION']) == false)
+	    		return generate_response(401, {'TYPE' => resource.auth_type, 'REALM' => resource.auth_realm})
+	        # Return 403 if the resource is protected
+	        #return generate_response(403, {})
+	    	else
+	        	# Return 404 if file not exist
+		        file_path = resource.resolve_path
+		        return generate_response(404, {}) unless File.exist?(file_path)
+
+		        puts "----------- RETURN FILE BEGIN ---------------------"
+		        puts "Server return file " + file_path
+		        puts "----------- RETURN FILE END -----------------------"
+
+		        # Handle script and resource
+		        if resource.script_aliased?
+		        	puts "Got script"
+		        	result = IO.popen(file_path)
+		        else
+		        	result = File.open(file_path, 'r')
+		        end
+		        # Return file content 
+		        return generate_response(200, {'BODY' => result.read})
+		    end
+		end
+
+		def generate_response (code, options)
+			Response::Factory.create(code, options)
+		end
+	end
 end
--- web_server.rb
+++ web_server.rb
@@ -5,35 +5,35 @@ end
 require 'ruby-debug'
 
 module WebServer
-  class Server
-    attr_accessor :httpd_conf, :mime_conf
 
+  class Server
     def initialize(options={})
       # Set up WebServer's configuration files and logger here
+      # Set up global variable config_file and mime_file
       config_file = File.open("config/httpd.conf", 'r')
-      @httpd_conf = HttpdConf.new(config_file.read)
+      $httpd_conf = HttpdConf.new(config_file.read)
       mime_file = File.open("config/mime.types", "r")
-      @mime_conf = MimeTypes.new(mime_file.read)
-      #puts @httpd_conf.inspect
-      #puts @mime_conf.inspect
+      $mime_conf = MimeTypes.new(mime_file.read)
 
       # Do any preparation necessary to allow threading multiple requests
     end
 
     def start
       # Initialize a tcp server
-      STDERR.puts "Listen to port " + @httpd_conf.port.to_s
-      server = TCPServer.new('localhost', @httpd_conf.port)
+      STDERR.puts "Listen to port " + $httpd_conf.port.to_s
+      server = TCPServer.new('localhost', $httpd_conf.port)
 
 
       loop do
         Thread.start(server.accept) do |client|
-          worker(client)
+          worker = Worker.new(client, nil)
+          worker.process_request
+          # Close the socket, terminating the connection
           client.close
         end
       end
 
-      # Close the socket, terminating the connection
+
 
 
       # Begin your 'infinite' loop, reading from the TCPServer, and
@@ -42,67 +42,7 @@ module WebServer
 
     private
 
-    # thread function
-    def worker (socket)
-        request = Request.new(socket)
 
-        response = handle_message(request)
-
-        puts "---------- Response Message BEGIN -------------------"
-        puts response.header
-        puts "---------- Response Message END ---------------------"
-        socket.print response.to_s
-    end
-
-    def handle_message (request)
-      response = ""
-
-      case request.http_method
-      when "GET"
-        response = send_resource(request)
-      when "HEAD"
-        response = generate_response(200, {})
-      when "POST"
-        response = generate_response(200, {})
-      when "PUT"
-        response = generate_response(200, {})
-      else
-        response = generate_response(404, {}) 
-      end                  
-    end
-
-    def send_resource (request)   
-      resource = Resource.new(request, @httpd_conf, @mime_conf)
-      if resource.protected? && (resource.authorized?(request.headers['AUTHORIZATION']) == false)
-
-        return generate_response(401, {'TYPE' => resource.auth_type, 'REALM' => resource.auth_realm})
-        # Return 403 if the resource is protected
-        #return generate_response(403, {})
-      else
-        # Return 404 if file not exist
-        file_path = resource.resolve_path
-        return generate_response(404, {}) unless File.exist?(file_path)
-
-        puts "----------- RETURN FILE BEGIN ---------------------"
-        puts "Server return file " + file_path
-        puts "----------- RETURN FILE END -----------------------"
-
-        # Handle script and resource
-        if resource.script_aliased?
-          puts "Got script"
-          result = IO.popen(file_path)
-        else
-          result = File.open(file_path, 'r')
-        end
-        # Return file content 
-        return generate_response(200, {'BODY' => result.read})
-      end
-
-    end
-
-    def generate_response (code, options)
-      Response::Factory.create(code, options)
-    end
   end
 
 
